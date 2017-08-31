df9e1aceb0a2d585ff7fd20a4967bbb3f795ee78
kpryzk <kepryzk@gmail.com>
2017-08-31 14:35:51 -0700

    what

---
+++ lab-katherine/route/route-toy.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const Toy = require('../model/toy')
+const storage = require('../lib/storage')
+const response = require('../lib/response')
+const debug = require('debug')('http:route-toy')
+
+module.exports = function(router) {
+  router.post('/api/toy', (req, res) => {
+    debug('/api/toy POST')
+    try {
+      let newToy = new Toy(req.body.name, req.body.desc)
+      storage.create('toy', newToy)
+        .then(toy => response.sendJson(res, 201, toy))
+    } catch(e) {
+      console.error(e)
+      response.sendText(res, 400, `bad request: ${e.message}`)
+    }
+  })
+
+  router.get('/api/toy', (req, res) => {
+    debug('/api/toy GET')
+    if(req.url.query._id) {
+      storage.fetchOne('toy', req.url.query._id)
+        .then(toy => {
+          response.sendJson(res, 200, toy)
+        })
+        .catch(err => {
+          console.error(err)
+          response.sendText(res, 400, `bad request; could not find record, ${err.message}`)
+        })
+      return
+    }
+    response.sendText(res, 400, 'bad request; item id required to get record')
+  })
+
+  router.put('/api/toy', (req, res) => {
+    debug('/api/toy PUT')
+    debugger;
+    if (req.url.query._id) {
+      if(!req.body._id && !req.body.name && !req.body.desc) {
+        response.sendJson(res, 400, `bad request: body improperly formatted`)
+        return
+      }
+      storage.update('toy', req.body)
+        .then(() => {
+          res.writeHead(204, {'Content-Type': 'text/plain'})
+          res.end()
+          return
+        })
+        .catch(err => {
+          response.sendJson(res, 400, `bad request; ${err.message}`)
+          return
+        })
+      return
+    }
+    response.sendJson(res, 400, 'bad request; item id required to get record')
+  })
+}
