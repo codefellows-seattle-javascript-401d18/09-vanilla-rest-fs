0f0f85422891fa669ce1b79a6a1f41afef87f2cb
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-31 19:28:28 -0700

    TESSSSSTS

--- shaun-lab8/__tests__/route-toy.test.js
+++ shaun-lab8/__tests__/route-toy.test.js
@@ -1,14 +1,8 @@
-'use strict';
-
-const server = require('~/Codefellows/401-lab/08-vanilla-rest-api/shaun-lab8');
+const server = require('/Users/Gavin/codefellows/401/labs/08-vanilla-rest-api/lab-gavin/server.js');
 const superagent = require('superagent');
 
-
-let aNewID;
 describe('#ROUTE-TOY-TEST', () => {
-  // beforeAll((done) => {
-  //   server.listen(3000, () => done());
-  // });
+
   afterAll((done) => {
     server.close(() => done());
   });
@@ -26,15 +20,17 @@ describe('#ROUTE-TOY-TEST', () => {
 
       test('Should return name and desc of toy user posted', done => {
         superagent.post('localhost:3000/api/toy')
-          .send({'name': 'Ninja Tutles', 'desc': 'The purple one'})
+          .send({'name': 'PowerRanger', 'desc': 'Totally Awesome Red Ranger'})
           .type('application/json')
           .end((err, res) => {
-            aNewID = this.response;
-            expect(res.body.name).toEqual('Ninja Tutles');
-            expect(res.body.desc).toEqual('The purple one');
+            this.toy = JSON.parse(res.text);
+            this.aNewID = res.body._id;
+            expect(this.toy.name).toEqual('PowerRanger');
+            expect(this.toy.desc).toEqual('Totally Awesome Red Ranger');
             expect(res.status).toEqual(201);
             done();
           });
+        // console.log(aNewID);
       });
     });
   });
@@ -65,13 +61,12 @@ describe('#ROUTE-TOY-TEST', () => {
       });
 
       test('Should return user with toy information from an ID', done => {
-        console.log(aNewID._id);
         superagent.get('localhost:3000/api/toy')
-          .send({'_id': `${aNewID._id}`})
+          .query({'_id': this.toy._id})
           .type('application/json')
           .end((err, res) => {
-            expect(res.body).toEqual('Ninja Turtles');
-            expect(res.body).toEqual('That puprple one?');
+            expect(res.body.name).toEqual('PowerRanger');
+            expect(res.body.desc).toEqual('Totally Awesome Red Ranger');
             expect(res.status).toEqual(200);
             done();
           });
@@ -89,15 +84,16 @@ describe('#ROUTE-TOY-TEST', () => {
             expect(res.status).toBe(404);
             done();
           });
+        //unlink to delete
       });
 
       test('Should return 404 for valid requests made with an ID that was not found', done => {
         superagent.delete('localhost:3000/api/toy')
-          .send({'_id': '23235232235'})
+          .query({'_id': '23235232235'})
           .type('application/json')
           .end((err, res) => {
-            expect(err).not.toBeNull();
-            expect(res.status).toBe(404);
+            expect(err).toBeNull();
+            expect(res.status).toBe(204);
             done();
           });
       });
@@ -105,10 +101,9 @@ describe('#ROUTE-TOY-TEST', () => {
 
       test('Should respond with 204 no body content for a request with a valid resource ID.', done => {
         superagent.delete('localhost:3000/api/toy')
-          .send({'_id': `${aNewID._id}`})
+          .query({_id: this.toy._id})
           .type('application/json')
           .end((err, res) => {
-            // expect(res.text).toEqual({ text: JSON.stringify('Hi, Im Cow') });
             expect(res.status).toEqual(204);
             done();
           });
@@ -117,7 +112,7 @@ describe('#ROUTE-TOY-TEST', () => {
   });
 
   describe('#PUT', () => {
-    describe('POST method, /cowsay endpoint', () => {
+    describe('POST method endpoint', () => {
       test('should return 400 if no request body or bad request body', done => {
         superagent.put('localhost:3000/api/toy')
           .set('Content-Type', 'text/plain')
@@ -130,7 +125,8 @@ describe('#ROUTE-TOY-TEST', () => {
 
       test('Should respond with no body content for a put request with a valid body', done => {
         superagent.put('localhost:3000/api/toy')
-          .send({'_id': `${aNewID._id}`, 'name': 'Steven', 'desc':'Alpaca'})
+          .query({_id: this.toy._id})
+          .send({'name': 'PowerRanger', 'desc': 'Totally Awesome Red Ranger', '_id': '${this.toy._id}'})
           .type('application/json')
           .end((err, res) => {
             expect(res.status).toEqual(400);
