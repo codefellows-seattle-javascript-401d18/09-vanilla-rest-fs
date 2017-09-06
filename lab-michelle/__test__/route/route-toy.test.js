e8794a9392ceec3c9b549aa37ba89388f76e3b45
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-05 18:30:22 -0700

    tried to come up with some ideas for put

--- lab-michelle/__test__/route/route-toy.test.js
+++ lab-michelle/__test__/route/route-toy.test.js
@@ -72,50 +72,70 @@ describe('Testing toy routes', function() {
               done();
             });
         });
-        test('should not ')
+        describe('Invalid requests', () => {
+          test('should not get a valid thing', done => {
+            superagent.get(':3000/api/toy')
+              .query({_id: this.mockToy._id})
+              .then(res => {
+                this.resGet = res.body;
+                this.resGet.status = res.status;
+                expect(this.resGet).toBeInstanceOf(woot);
+                expect(this.resGet).toHaveProperty('banana');
+                expect(this.resGet).toHaveProperty('bob');
+                expect(this.resGet.status).toBe(404);
+                done();
+              });
+          });
+        });
       });
     });
-  });
-  describe('PUT requests', ()=> {
-    describe('Valid requests', () => {
-      beforeAll(done => {
-        superagent.get(':3000/api/toy')
-          .query({_id: '58465d51-2bcc-470c-9895-d97a7b71d7ad'})
-          .then(res => {
-            this.resGet = res;
-            expect(this.mockToy._id).toBeTruthy();
-            done();
-          });
-      });
-      test('should get the record from the toy dir', done => {
-        fs.readdirProm(`${__dirname}/../../data/toy`)
-          .then(files => {
-            let expectedFalse = files.includes(`${this.mockToy._id}.json`);
-            expect(expectedFalse).toBeFalsy();
-            console.log(expectedFalse);
-            done();
-          });
+    describe('PUT requests', ()=> {
+      describe('Valid requests', () => {
+        test('should create a return a new toy, given a valid request', done => {
+          superagent.put(':3000/api/toy')
+            .type('application/json')
+            .send({
+              name: 'jones',
+              desc: 'stuffed turtle',
+            })
+            .then(res => {
+              this.mockToy = res.body;
+              this.resPut = res;
+
+              //if jones the turtle exists, then update (so mock data = the actual data that goes in)
+              //if jones the turtle doesn't exist, create him
+              if (!this.mockToy) {
+                expect(this.mockToy).toBeInstanceOf(Object);
+                expect(this.mockToy).toHaveProperty('name');
+                expect(this.mockToy).toHaveProperty('desc');
+                expect(this.mockToy).toHaveProperty('_id');
+                done();
+              } else {
+                expect(this.mockToy.name).toBe('jones');
+              }
+            });
+        });
       });
     });
-  });
-  describe('DELETE requests', ()=> {
-    describe('Valid requests', () => {
-      beforeAll(done => {
-        superagent.delete(':3000/api/toy')
-          .query({_id: this.mockToy._id})
-          .then(res => {
-            this.resDelete = res;
-            done();
-          });
-      });
-      test('should remove the record from the toy dir', done => {
-        fs.readdirProm(`${__dirname}/../../data/toy`)
-          .then(files => {
-            let expectedFalse = files.includes(`${this.mockToy._id}.json`);
-            expect(expectedFalse).toBeFalsy();
-            console.log(expectedFalse);
-            done();
-          });
+    describe('DELETE requests', ()=> {
+      describe('Valid requests', () => {
+        beforeAll(done => {
+          superagent.delete(':3000/api/toy')
+            .query({_id: this.mockToy._id})
+            .then(res => {
+              this.resDelete = res;
+              done();
+            });
+        });
+        test('should remove the record from the toy dir', done => {
+          fs.readdirProm(`${__dirname}/../../data/toy`)
+            .then(files => {
+              let expectedFalse = files.includes(`${this.mockToy._id}.json`);
+              expect(expectedFalse).toBeFalsy();
+              console.log(expectedFalse);
+              done();
+            });
+        });
       });
     });
   });
